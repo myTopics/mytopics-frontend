0ca63836e02c0e11ddd0578b7c00b180337bb2c4
Timothy Haberthuer <thaberthuer@gmail.com>
2019-09-28 14:25:22 +0200

    Animate Cards PoC

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import {Card, StyledBody, StyledThumbnail} from 'baseui/card';
+import React, {useState} from 'react';
 import { ArticleInterface } from '../../interfaces/article.interface';
 import './Article.css';
+import {Card} from "baseui/card";
+import {Flipper, Flipped} from "react-flip-toolkit";
 
 const Article: React.FC<ArticleInterface> = (props) => {
-  return (
-      <Card>
-          <div className={'container'}>
-              <div className={'flex-2'}>
-                  <h1 className={'card-title'}>
-                      {props.title}
-                  </h1>
-                <span className={'card-summary'}>
-                    {props.summary}
-                </span>
-              </div>
-              <div className={'flex1'}>
-                  <img src={props.thumbnail}/>
-              </div>
-          </div>
-      </Card>
+
+    const [fullScreen, setFullScreen] = useState(false);
+    const toggleFullScreen = () => setFullScreen(prevState => !prevState);
+
+    return (
+        <Flipper flipKey={fullScreen}>
+            <Flipped flipId={'article'}>
+                <div className={fullScreen ? 'full-screen' : 'normal'} onClick={toggleFullScreen}>
+                    <Card>
+                        <div className={'container'}>
+                            <div className={'flex-2'}>
+                                <h1 className={'card-title'}>
+                                    {props.title}
+                                </h1>
+                                <span className={'card-summary'}>
+                                    {props.summary}
+                                </span>
+                            </div>
+                            <div className={'flex1'}>
+                                <img src={props.thumbnail} alt={props.title}/>
+                            </div>
+                        </div>
+                    </Card>
+                </div>
+            </Flipped>
+        </Flipper>
   );
 };
 

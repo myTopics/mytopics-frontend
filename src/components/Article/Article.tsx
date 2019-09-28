4ddbbdef28b8baa06dd8063c510c9d9a20559f81
Timothy Haberthuer <thaberthuer@gmail.com>
2019-09-28 11:19:24 +0200

    Create own card style (with blackjack and hookers)

--- src/components/Article/Article.tsx
+++ src/components/Article/Article.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import {Card, StyledBody, StyledThumbnail} from 'baseui/card';
 import { ArticleInterface } from '../../interfaces/article.interface';
+import './Article.css';
 
 const Article: React.FC<ArticleInterface> = (props) => {
   return (
-      <Card title={props.title}>
-          <StyledBody>
-              {props.summary}
-              <StyledThumbnail src={props.thumbnail} />
-          </StyledBody>
+      <Card>
+          <div className="container">
+              <div className="flex-2">
+                  <h1>
+                      {props.title}
+                  </h1>
+                <span>
+                    {props.summary}
+                </span>
+              </div>
+              <div className="flex1">
+                  <img src={props.thumbnail}/>
+              </div>
+          </div>
       </Card>
   );
 };
